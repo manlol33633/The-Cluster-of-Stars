0d34a7cc837de3f0f5ef3a0b4f9d5c910a6de746
Ethan Giang <51886476+manlol33633@users.noreply.github.com>
2023-04-24 08:05:14 -0500

    Deco

---
+++ Assets/Scripts/Planetary Gravity.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetaryGravity : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
