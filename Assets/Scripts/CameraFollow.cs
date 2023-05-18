cd2f65c88d0972ff9b356c2ce1da810c00e51b2f
Ethan Giang <51886476+manlol33633@users.noreply.github.com>
2023-05-18 11:22:32 -0500

    ?WEAMNfuise

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private GameObject playerTransform;
+    private GameObject player;
+    private RaycastHit hit;
 
     void Start()
     {
         playerTransform = GameObject.Find("Orientation");
+        player = GameObject.Find("Player");
     }
 
     void Update()
     {
-        transform.position = playerTransform.transform.forward * -10 + playerTransform.transform.position;
+        RaycastHit hit;
+        Ray ray = new Ray(player.transform.position, -player.transform.forward);
+        Physics.Raycast(ray , out hit, 10f);
+        Vector3 hitPoint = hit.point;
+        
+        if (Physics.Raycast(player.transform.position, -player.transform.forward, 10f)) {
+            transform.position = hitPoint;
+        } else {
+            transform.position = playerTransform.transform.forward * -10 + playerTransform.transform.position;
+        }
         transform.rotation = playerTransform.transform.rotation;
+        
     }
 }
